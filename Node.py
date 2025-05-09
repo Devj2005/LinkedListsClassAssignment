b8ab17f70c27733e0554c8d3d2d2a26d5335ed73
Devyan Jethwa A <vrindevj@gmail.com>
2025-05-09 11:17:25 +0300

    Node file for the lists

---
+++ Node.py
@@ -0,0 +1,8 @@
+#It is the building block of the list itself.
+
+
+class Node:
+    def __init__(self, val):
+        self.val = val #stores the values.
+        self.next = None # points to the next node.
+
